3022a6e54fb40aa6cc801f62faaa2bf54de26cea
Goutham Nalla <ngoutham179@gmail.com>
2024-01-12 10:57:45 -0600

    OOPS

---
+++ Department.java
@@ -0,0 +1,20 @@
+package Abstraction;
+
+abstract class Department {
+    int size = 100;
+    abstract int getDepartmentSize();
+}
+
+class Deptsize extends Department{
+    public int getDepartmentSize()
+    {
+        return size;
+
+    }
+}
+class Main{
+    public static void main(String args[]) {
+        Deptsize dept = new Deptsize();
+        System.out.println(dept.getDepartmentSize());
+    }
+}
---
+++ Product.java
@@ -0,0 +1,23 @@
+package Abstraction;
+
+public class Product {
+    double price = 100;
+    //getter and setter methods
+    public double getPrice()
+    {
+        return price;
+    }
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+    public double getPrice(int quantity)
+    {
+        return price*quantity;
+    }
+    public static void main(String args[]){
+        Product p = new Product();
+        double totalquantity = p.getPrice(2);
+        System.out.println(totalquantity);
+    }
+}
