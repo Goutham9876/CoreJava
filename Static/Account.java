6f97e156908b010f82b432904c9f7dfdc3c8ff7d
Goutham Nalla <ngoutham179@gmail.com>
2024-01-15 21:48:00 -0600

    Fianl

---
+++ Static/Account.java
@@ -0,0 +1,13 @@
+package Abstraction.Static;
+
+public class Account {
+    static int accountNumber = 12233;
+
+    public static int getAccountNumber(){
+        return accountNumber;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getAccountNumber());
+    }
+}
